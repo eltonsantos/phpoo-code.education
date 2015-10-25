834ef1eda23b98dd0fce5283abd6891194be41bb
Elton Santos <eltonaxl@hotmail.com>
2015-10-25 17:20:15 -0300

    Segundo commit

---
+++ Cliente.php
@@ -0,0 +1,24 @@
+<?php
+
+class Cliente
+{
+    public $nome;
+    public $cpf;
+    public $endereco;
+}
+
+$clientes = [$nome => ["Elton", "Lucivania", "Moroni", "Filipe", "Eloy", "Vânia", "Ericson", "Thyago", "Laércio", "Lucivan"], $cpf => ["02849112321" , "111111", "222222", "333333", "444444", "555555", "6666666", "7777777", "8888888", "999999"], $endereco => ["Rua 1", "Rua 2", "Rua 3", "Rua 4", "Rua 5", "Rua 6", "Rua 7", "Rua 8", "Rua 9", "Rua 10"]];
+
+/*
+$cliente1 = new Cliente();
+$cliente1->nome = "Elton";
+$cliente2 = new Cliente();
+$cliente3 = new Cliente();
+$cliente4 = new Cliente();
+$cliente5 = new Cliente();
+$cliente6 = new Cliente();
+$cliente7 = new Cliente();
+$cliente8 = new Cliente();
+$cliente9 = new Cliente();
+$cliente10 = new Cliente();
+*/
--- index.php
+++ index.php
@@ -1,12 +1,35 @@
 <html>
     <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
         <title>Projeto PHP OO - Code.Education</title>
         <link rel="stylesheet" href="css/bootstrap.min.css" />
         <link rel="stylesheet" href="css/style.css" />
     </head>
     <body>
 
-        <h1>Hello World</h1>
+        <?php require_once "Cliente.php"; ?>
+        <div class="container">
+            <h1>Hello World</h1>
+            <table class="table table-bordered table-condensed table-hover">
+                <thead>
+                    <th>Nome</th>
+                    <th>CPF</th>
+                    <th>Endereço</th>
+                </thead>
+                <tbody>
+                    <?php for($cliente) { ?>
+                    <tr>
+                        <td><?php $clientes[$i]->nome; ?></td>
+                        <td><?php $clientes[$i]->cpf; ?></td>
+                        <td><?php $clientes[$i]->endereco; ?></td>
+                    </tr>
+                    <?php } ?>
+                </tbody>
+            </table>
+        </div>
+
 
     </body>
 </html>
